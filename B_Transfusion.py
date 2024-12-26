ee6622bab55fe9e4195479cda914865696f239b8
dhamer69 <avishmakar99@gmail.com>
2024-12-26 20:00:08 +0530

    Update B_Transfusion.py

--- B_Transfusion.py
+++ B_Transfusion.py
@@ -6,6 +6,10 @@ while(t>0):
     s1=0
     ct1=0
     ct0=0
+    # s0 sum of even place 
+    # s1 sum of odd places.
+    # ct1 no of odd place/index
+    # ct0 no of even places
     for i in range(n):
         if i%2==0:
             s0+=a[i]
@@ -13,6 +17,8 @@ while(t>0):
         else :
             s1+=a[i]
             ct1+=1
+
+    # dis is of num in odd and even place condns
     if s0*ct1==s1*ct0 and (s0+s1)%n==0:
         print("YES")
     else:
